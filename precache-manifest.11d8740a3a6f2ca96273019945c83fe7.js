419d4f19fd75741ec32072db3f0d15ea29d63421
Andrew Gormley <Andrew@Andrews-MacBook-Pro-2.local>
2020-03-19 18:53:11 +0000

    Updates

---
+++ precache-manifest.11d8740a3a6f2ca96273019945c83fe7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bba2e85c0ae277cbfe7070fd743e1a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a8c43d48300da8c30934",
+    "url": "/portfolio/static/css/main.9ccae4a8.chunk.css"
+  },
+  {
+    "revision": "b44ba3414bed70bb9446",
+    "url": "/portfolio/static/js/2.3861b8c5.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/portfolio/static/js/2.3861b8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c43d48300da8c30934",
+    "url": "/portfolio/static/js/main.f13f4e9f.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "c0324d9930619504bf423bd0a2ae9910",
+    "url": "/portfolio/static/media/1.c0324d99.jpg"
+  },
+  {
+    "revision": "255aa7afe3ddb52c11cba9c3a9df5919",
+    "url": "/portfolio/static/media/2.255aa7af.jpg"
+  },
+  {
+    "revision": "3d532f0d9f3350c6157a9e84af7c6d8e",
+    "url": "/portfolio/static/media/3.3d532f0d.jpg"
+  },
+  {
+    "revision": "21603ff4dd7360a845d23562f66de77b",
+    "url": "/portfolio/static/media/4.21603ff4.jpg"
+  },
+  {
+    "revision": "28c5ade537e5e6da314373fd20e19ea0",
+    "url": "/portfolio/static/media/5.28c5ade5.jpg"
+  },
+  {
+    "revision": "d8af470e44be6c1d2d07dee84db355f6",
+    "url": "/portfolio/static/media/JetBrainsMono-Bold.d8af470e.ttf"
+  },
+  {
+    "revision": "e1caef645de334fee2f25834b0d03c28",
+    "url": "/portfolio/static/media/JetBrainsMono-Regular.e1caef64.ttf"
+  },
+  {
+    "revision": "0633d79ec446fb097276a414053f8315",
+    "url": "/portfolio/static/media/branding.0633d79e.jpg"
+  },
+  {
+    "revision": "7574553d03deb9962504b012850f1b46",
+    "url": "/portfolio/static/media/concept-homepage.7574553d.jpg"
+  },
+  {
+    "revision": "b8b03948dd11435962f692979ae09d3e",
+    "url": "/portfolio/static/media/crowdcube-cover.b8b03948.jpg"
+  },
+  {
+    "revision": "4b6af768b6ac8369279e96f3a8705515",
+    "url": "/portfolio/static/media/data.4b6af768.jpg"
+  },
+  {
+    "revision": "6198fb5f1ccb91d7e44da0ad23c9b4db",
+    "url": "/portfolio/static/media/design-team.6198fb5f.jpg"
+  },
+  {
+    "revision": "284c5f7b8268513333680cda164b2399",
+    "url": "/portfolio/static/media/header-illustration.284c5f7b.png"
+  },
+  {
+    "revision": "a51ce2a5c0a2cdf834b2ca28534eba4d",
+    "url": "/portfolio/static/media/ldn19-cover.a51ce2a5.jpg"
+  },
+  {
+    "revision": "084b9b1285c1772df471cd8c1b93ab7d",
+    "url": "/portfolio/static/media/live-homepage.084b9b12.jpg"
+  },
+  {
+    "revision": "4744f639b6b1f9c85ec8d39d9518af38",
+    "url": "/portfolio/static/media/live-list-page.4744f639.jpg"
+  },
+  {
+    "revision": "5c8852864f9fca636fed20390fd3aeec",
+    "url": "/portfolio/static/media/live-product-page.5c885286.jpg"
+  },
+  {
+    "revision": "4fd68fd18db2df70a427ce5285f46e4d",
+    "url": "/portfolio/static/media/new-homepage.4fd68fd1.jpg"
+  },
+  {
+    "revision": "816fb51246200b080733bb0ac87d1a11",
+    "url": "/portfolio/static/media/old-homepage.816fb512.jpg"
+  },
+  {
+    "revision": "e886903b7cf3b9cf6b36f0afe226f3fd",
+    "url": "/portfolio/static/media/old-homepage.e886903b.jpg"
+  },
+  {
+    "revision": "dbb6e943ef0505d3232133ad87fcb84f",
+    "url": "/portfolio/static/media/profile.dbb6e943.jpg"
+  },
+  {
+    "revision": "342d996016738c9d905d300f27b38878",
+    "url": "/portfolio/static/media/sweatshop-cover.342d9960.gif"
+  },
+  {
+    "revision": "df3cb998735d2757eaff8a2c44df9365",
+    "url": "/portfolio/static/media/whiteboard-wireframes.df3cb998.jpg"
+  },
+  {
+    "revision": "ec3d9964014092d6a07ffc4bca32949e",
+    "url": "/portfolio/static/media/wireframes.ec3d9964.jpg"
+  }
+]);
